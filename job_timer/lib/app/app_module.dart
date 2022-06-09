7a921cc4a6550f8a577bcb8321758fe40b77fc0b
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-09 09:32:55 -0300

    controller, database, service

--- job_timer/lib/app/app_module.dart
+++ job_timer/lib/app/app_module.dart
@@ -1,19 +1,32 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/core/database/database.dart';
+import 'package:job_timer/app/core/database/database_impl.dart';
 import 'package:job_timer/app/modules/home/home_module.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'package:job_timer/app/services/auth/auth_service.dart';
+import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton<AuthService>((i) => AuthServiceImpl()),
+        Bind.lazySingleton<Database>((i) => DatabaseImpl())
+      ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute(
           '/',
-          child: ((context, args) => SplashPage()),
+          child: ((context, args) => const SplashPage()),
         ),
-        ModuleRoute('/login', module: LoginModule()),
-        ModuleRoute('/home', module: HomeModule()),
+        ModuleRoute(
+          '/login',
+          module: LoginModule(),
+        ),
+        ModuleRoute(
+          '/home',
+          module: HomeModule(),
+        ),
       ];
 }
---
+++ job_timer/lib/app/modules/login/controller/login_controller.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:job_timer/app/services/auth/auth_service.dart';
+
+part 'login_state.dart';
+
+class LoginController extends Cubit<LoginState> {
+  final AuthService _authService;
+
+  LoginController({required AuthService authService})
+      : _authService = authService,
+        super(const LoginState.initial());
+
+  Future<void> signIn() async {
+    try {
+      emit(state.copyWith(status: LoginStatus.loading));
+      await _authService.signIn();
+    } catch (e) {
+      emit(state.copyWith(
+          status: LoginStatus.failure, errorMessage: 'Erro ao realizar login'));
+    }
+  }
+}
