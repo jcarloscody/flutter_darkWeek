11a83b45fc9fc9f0423a69de10f66d1b124a71b5
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-10 01:21:13 -0300

    modulo project

--- job_timer/lib/app/app_module.dart
+++ job_timer/lib/app/app_module.dart
@@ -3,6 +3,7 @@ import 'package:job_timer/app/core/database/database.dart';
 import 'package:job_timer/app/core/database/database_impl.dart';
 import 'package:job_timer/app/modules/home/home_module.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/modules/project/project_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
 import 'package:job_timer/app/services/auth/auth_service.dart';
 import 'package:job_timer/app/services/auth/auth_service_impl.dart';
@@ -27,6 +28,10 @@ class AppModule extends Module {
         ModuleRoute(
           '/home',
           module: HomeModule(),
+        ),
+        ModuleRoute(
+          '/project',
+          module: ProjectModule(),
         ),
       ];
 }
--- job_timer/lib/app/modules/home/widgets/header_project_menu.dart
+++ job_timer/lib/app/modules/home/widgets/header_project_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/core/entities/project_status.dart';
 
 class HeaderProjectMenu extends SliverPersistentHeaderDelegate {
@@ -34,7 +35,9 @@ class HeaderProjectMenu extends SliverPersistentHeaderDelegate {
                 SizedBox(
                   width: constraints.maxWidth * 0.4,
                   child: ElevatedButton.icon(
-                    onPressed: () {},
+                    onPressed: () {
+                      Modular.to.pushNamed('/project/register');
+                    },
                     icon: const Icon(Icons.add),
                     label: const Text('Novo Projeto'),
                   ),
